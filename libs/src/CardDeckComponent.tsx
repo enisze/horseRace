7a621d924b217346fbe5bdedd148d22a92a21e48
Enis <eniszej@gmail.com>
2022-06-07 00:21:36 +0200

    ✨ Starting point asses

--- libs/src/CardDeckComponent.tsx
+++ libs/src/CardDeckComponent.tsx
@@ -1,14 +1,18 @@
 import React, { CSSProperties, FunctionComponent } from "react";
 
-import tw from "twrnc";
-import { View, Text, Image } from "react-native";
+import { View } from "react-native";
 import Card from "./Card";
+import { RankSymbol } from "../types/RankSymbol.type";
+import { map } from "lodash";
+import tw from "twrnc";
 
 export const CardDeckComponent: FunctionComponent = () => {
+  const startingCards: RankSymbol[] = ["AC", "AD", "AH", "AS"];
   return (
-    <View>
-      <Text style={tw`text-black`}>tessdwt</Text>
-      <Card rankSymbol="2H" />
+    <View style={tw`flex flex-row justify-center items-end`}>
+      {map(startingCards, (card) => {
+        return <Card rankSymbol={card} />;
+      })}
     </View>
   );
 };
