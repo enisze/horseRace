5a846286622e6e2c74eaa621c75ac707c5b56e54
Enis <eniszej@gmail.com>
2022-07-17 16:00:34 +0200

    ⏪

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+import { createTheme, ThemeProvider } from '@rneui/themed'
 import React from 'react'
 import { GameContextProvider } from './libs/src/helpers/GameContext'
 import { MainView } from './libs/src/layouts/MainView'
 import { StartView } from './libs/src/StartView'
 
+const theme = createTheme({
+  lightColors: {},
+  darkColors: {},
+})
+
 export default function App() {
   return (
-    <GameContextProvider>
-      <StartView />
-      <MainView />
-    </GameContextProvider>
+    <ThemeProvider theme={theme}>
+      <GameContextProvider>
+        <StartView />
+        <MainView />
+      </GameContextProvider>
+    </ThemeProvider>
   )
 }
