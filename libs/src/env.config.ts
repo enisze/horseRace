894c14159225d8e1047928318e67c4696bb0da78
Enis <eniszej@gmail.com>
2022-07-20 22:50:54 +0200

    ➕  react-native-dotenv

---
+++ libs/src/env.config.ts
@@ -0,0 +1,1 @@
+export const TEST = process.env.GOOGLE_ADMOB_PRODUCTION_KEY
--- libs/src/helpers/MainProvider.tsx
+++ libs/src/helpers/MainProvider.tsx
@@ -1,10 +1,13 @@
 import { setTestDeviceIDAsync } from 'expo-ads-admob'
 import React, { FunctionComponent, useEffect } from 'react'
+import { TEST } from '../env.config'
 
 const MainProvider: FunctionComponent = ({ children }) => {
   const test = async () => {
     await setTestDeviceIDAsync('EMULATOR')
   }
+
+  console.log(TEST)
 
   useEffect(() => {
     test()
