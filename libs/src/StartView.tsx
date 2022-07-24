842923d32f75b11d62e363ebe156c90f42404f0f
Enis <eniszej@gmail.com>
2022-07-25 00:07:59 +0200

    💄 🐛 ♻️ Mobile UI

--- libs/src/StartView.tsx
+++ libs/src/StartView.tsx
@@ -8,7 +8,7 @@ import { NewGameModal } from './layouts/NewGameModal'
 const paypalDonationURL =
   'https://www.paypal.com/donate/?hosted_button_id=Q7H2L2WCGZKVL'
 
-const buttonStyle = tw`w-40 mt-5`
+const buttonStyle = tw`w-40 mt-10`
 
 export const StartView: FunctionComponent = () => {
   const { levelAmount } = useGameContext()
@@ -22,29 +22,33 @@ export const StartView: FunctionComponent = () => {
 
   return (
     <View style={tw`flex flex-col justify-center items-center pt-20`}>
-      <Button
-        title="Start Game"
-        onPress={() => {
-          setShowModal(true)
-        }}
-        style={buttonStyle}
-      />
-      <Button title="Continue Game" style={buttonStyle} />
-      <Button
-        title={'Support me '}
-        onPress={donationsNavigate}
-        style={buttonStyle}
-        type="solid"
-        icon={
-          <Icon
-            color={'red'}
-            tvParallaxProperties={undefined}
-            name="heart"
-            type="entypo"
-          />
-        }
-        iconRight
-      />
+      <View style={buttonStyle}>
+        <Button
+          title="Start Game"
+          onPress={() => {
+            setShowModal(true)
+          }}
+        />
+      </View>
+      <View style={buttonStyle}>
+        <Button title="Continue Game" />
+      </View>
+      <View style={buttonStyle}>
+        <Button
+          title={'Support me '}
+          onPress={donationsNavigate}
+          type="solid"
+          icon={
+            <Icon
+              color={'red'}
+              tvParallaxProperties={undefined}
+              name="heart"
+              type="entypo"
+            />
+          }
+          iconRight
+        />
+      </View>
       <NewGameModal
         showModal={showModal}
         closeModal={() => setShowModal(false)}
