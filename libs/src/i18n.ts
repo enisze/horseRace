318c7d9abf66597f7be57a0eede8253e1c35bef5
Enis <eniszej@gmail.com>
2022-10-04 00:05:36 +0200

    ✨ Add internationalization

--- libs/src/i18n.ts
+++ libs/src/i18n.ts
@@ -1,8 +1,8 @@
 import * as Localization from 'expo-localization'
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import de from '../../locales/de/horseRace.json'
-import en from '../../locales/en/horseRace.json'
+import de from '../locales/de/horseRace.json'
+import en from '../locales/en/horseRace.json'
 
 i18next.use(initReactI18next).init({
   resources: {
