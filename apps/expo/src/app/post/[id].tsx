6fb7f7d725f5f042bcabab7ea4cadb10407492d2
Enis <eniszej@gmail.com>
2023-09-10 04:26:41 +0200

    🐛

--- apps/expo/src/app/post/[id].tsx
+++ apps/expo/src/app/post/[id].tsx
@@ -1,5 +1,5 @@
-import { SafeAreaView, Text, View } from "react-native";
-import { Stack, useGlobalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native";
+import { useGlobalSearchParams } from "expo-router";
 
 export default function Post() {
   const { id } = useGlobalSearchParams();
@@ -11,11 +11,11 @@ export default function Post() {
 
   return (
     <SafeAreaView className="bg-[#1F104A]">
-      <Stack.Screen options={{ title: data.title }} />
+      {/* <Stack.Screen options={{ title: data.title }} />
       <View className="h-full w-full p-4">
         <Text className="py-2 text-3xl font-bold text-white">{data.title}</Text>
         <Text className="py-4 text-white">{data.content}</Text>
-      </View>
+      </View> */}
     </SafeAreaView>
   );
 }
