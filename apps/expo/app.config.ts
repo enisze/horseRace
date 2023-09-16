4bf63689adb08f7d6cc0fba0476db1c4c53ff623
Enis <eniszej@gmail.com>
2023-09-16 03:48:51 +0200

    ⚗️

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -36,6 +36,7 @@ const defineConfig = (): ExpoConfig => ({
       foregroundImage: './assets/icon.png',
       backgroundColor: '#1F104A',
     },
+    versionCode: 7,
     permissions: [],
   },
   // extra: {
